abf49781a96a66220f6b7c6b5f09ad8a4452c2f4
alexisduque <alexisd61@gmail.com>
2014-10-09 12:44:06 +0200

    Init github repository

---
+++ README.md
@@ -0,0 +1,8 @@
+#UTT Report Android App
+-----
+
+Cette application a été développée & testée sur une **tablette de 10"** et **Android 4.2.2** (SDK 17)
+
+
+ * Pour avoir les ITEMS(Stories/Argumentation/Communication...) Veuillez telecharger le contenu du dossier ITEMS et de l'inclure dans le dossier /MM/ITEMS sur la tablette.
+
---
+++ UTT-Report/src/core/Tools.java
@@ -0,0 +1,43 @@
+package core;
+
+import java.util.Calendar;
+
+import uttreport.ui.R;
+
+import android.content.Intent;
+import android.text.Html;
+import android.util.Log;
+
+public class Tools {
+	public final static String meetings_directory = "/storage/emulated/legacy/MM/MEETINGS";
+	public final static String items_directory = "/storage/emulated/legacy/MM/ITEMS";
+	public final static String extra_directory = "/storage/emulated/legacy/EXTRA";
+	public static void show(String data)
+	{
+	    Log.e("stuff", data);
+	}
+	public void addCal()
+	{
+		Calendar cal = Calendar.getInstance();              
+		Intent intent = new Intent(Intent.ACTION_EDIT);
+		intent.setType("vnd.android.cursor.item/event");
+		intent.putExtra("beginTime", cal.getTimeInMillis());
+		intent.putExtra("allDay", true);
+		intent.putExtra("rrule", "FREQ=YEARLY");
+		intent.putExtra("endTime", cal.getTimeInMillis()+60*60*1000);
+		intent.putExtra("title", "A Test Event from android app");
+		//startActivity(intent);
+	}
+	public void sendMail()
+	{
+		Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND_MULTIPLE);
+        shareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject");
+        CharSequence seq = Html.fromHtml("<html></html>");
+        shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, seq);
+        shareIntent.setType("application/pdf");
+
+        shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, null);
+        
+	}
+
+}
